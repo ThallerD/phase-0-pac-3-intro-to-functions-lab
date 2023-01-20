54e61b4feb7f476d587b4281c50e73e0fc34f8bd
David Thaller <golf.nautica@gmail.com>
2023-01-19 19:49:11 -0800

    commit tree 5ebe914cb958928859ecbe0a6140627fb1db89f3

--- test/indexTest.js
+++ test/indexTest.js
@@ -7,11 +7,13 @@ describe('shout(string)', function() {
   })
 })
 
+
 describe('whisper(string)', function() {
   it('receives one argument and returns it in all lowercase', function() {
     expect(whisper('HELLO')).toEqual('hello');
   })
 })
+
 
 describe('logShout(string)', function() {
   it('takes a string argument and logs it in all caps using console.log()', function() {
@@ -19,11 +21,15 @@ describe('logShout(string)', function() {
 
     logShout('hello');
 
+
+
     expect(spy).toHaveBeenCalledWith('HELLO');
 
     console.log.restore();
   })
 })
+
+
 
 describe('logWhisper(string)', function() {
   it('takes a string argument and logs it in all lowercase using console.log()', function() {
@@ -31,11 +37,19 @@ describe('logWhisper(string)', function() {
 
     logWhisper('HELLO');
 
+
+
     expect(spy).toHaveBeenCalledWith('hello');
 
     console.log.restore();
   })
 })
+
+
+ 
+
+
+
 
 describe('sayHiToHeadphonedRoommate(string)', function() {
   it('returns "I can\'t hear you!" if `string` is lowercase', function() {
@@ -48,7 +62,8 @@ describe('sayHiToHeadphonedRoommate(string)', function() {
 
   it('returns "I would love to!" if `string` is "Let\'s have dinner together!"`', function () {
     expect(sayHiToHeadphonedRoommate("Let's have dinner together!")).toEqual(
-      "I would love to!"
-    );
+      "I would love to!")
   });
 })
+
+
